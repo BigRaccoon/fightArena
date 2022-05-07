a8c854854a6c57fb2644322b9a196c0181389bc2
NikaNika <55001607+BigRaccoon@users.noreply.github.com>
2022-05-07 22:07:36 +0300

    v 0.0.2

--- scripts.js
+++ scripts.js
@@ -1,1 +1,27 @@
 console.log("Hello worlds");
+
+//Переход между страницами
+const firstPageBtn = document.querySelector(".mainMenuButton");
+firstPageBtn.addEventListener("click", () => {
+  if (playerFighter) {
+    window.location.href = "fightArena.html";
+  }
+});
+
+//Выбор бойца
+const fighters = document.querySelectorAll(".fighterPhoto");
+const warrior = document.querySelector("#warrior");
+const berserk = document.querySelector("#berserk");
+const wizzard = document.querySelector("#wizzard");
+
+choosingFighter(warrior);
+choosingFighter(berserk);
+choosingFighter(wizzard);
+
+function choosingFighter(fighterClass) {
+  fighterClass.addEventListener("click", function () {
+    playerFighter = fighterClass.id;
+    console.log(playerFighter);
+    alert(`Вы выбрали класс: ${playerFighter}`);
+  });
+}
